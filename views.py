fc4e94ace759f24654b7f8a670cfcb2406ed1f2a
Matheus Portela <matheus.v.portela@gmail.com>
2015-11-29 13:47:06 -0200

    CRUD interface.  Please enter the commit message for your changes. Lines starting

--- views.py
+++ views.py
@@ -17,8 +17,16 @@ def index_view():
     except:
         return traceback.format_exc()
 
-@app.route('/surgery_types', methods=['GET', 'POST'])
-def surgery_types_view():
+@app.route('/surgery_types', methods=['GET'])
+def surgery_types():
+    if request.method == 'GET':
+        try:
+            return render_template('surgery_type_list.html')
+        except:
+            return traceback.format_exc()
+
+@app.route('/surgery_type/create', methods=['GET', 'POST'])
+def surgery_types_create_view():
     if request.method == 'GET':
         try:
             return render_template('surgery_type_create.html')
@@ -37,8 +45,8 @@ def surgery_types_view():
         except:
             return traceback.format_exc()
 
-@app.route('/surgery_type/<int:pk>', methods=['GET', 'PATCH', 'DELETE'])
-def surgery_type_view(pk):
+@app.route('/surgery_type/<int:pk>', methods=['GET'])
+def surgery_type_details_view(pk):
     if request.method == 'GET':
         try:
             surgery_type = models.SurgeryTypeModel(pk=pk)
@@ -47,44 +55,39 @@ def surgery_type_view(pk):
                 surgery_type=surgery_type)
         except:
             return traceback.format_exc()
-    elif request.method == 'PATCH':
-        return 'ECHO: PATCH\n'
-    elif request.method == 'DELETE':
-        return 'ECHO: DELETE\n'
 
-@app.route('/surgeries', methods=['GET', 'POST'])
-def surgeries_view():
+@app.route('/surgery_type/<int:pk>/edit', methods=['GET', 'POST'])
+def surgery_type_edit_view(pk):
     if request.method == 'GET':
         try:
-            return render_template('surgery_create.html')
+            surgery_type = models.SurgeryTypeModel(pk=pk)
+            surgery_type.load()
+            return render_template('surgery_type_edit.html',
+                surgery_type=surgery_type)
         except:
             return traceback.format_exc()
     elif request.method == 'POST':
         try:
-            surgery = models.SurgeryModel(
-                pk=int(request.form['pk']),
-                surgery_date=request.form['surgery_date'],
-                surgery_type_pk=request.form['surgery_type_pk'])
-            surgery.create()
+            surgery_type = models.SurgeryTypeModel(pk=pk)
+            surgery_type.name = request.form['name']
+            surgery_type.specialty = request.form['specialty']
+            surgery_type.description = request.form['description']
+            surgery_type.save()
             response = Response(response='Success', status=200)
             return response
         except:
             return traceback.format_exc()
 
-@app.route('/surgery/<int:pk>', methods=['GET', 'PATCH', 'DELETE'])
-def surgery_view(pk):
+@app.route('/surgery_type/<int:pk>/delete', methods=['GET'])
+def surgery_type_delete_view(pk):
     if request.method == 'GET':
         try:
-            surgery = models.SurgeryModel(pk=pk)
-            surgery.load()
-            return render_template('surgery_details.html',
-                surgery=surgery)
+            surgery_type = models.SurgeryTypeModel(pk=pk)
+            surgery_type.delete()
+            response = Response(response='Success', status=200)
+            return response
         except:
             return traceback.format_exc()
-    elif request.method == 'PATCH':
-        return 'ECHO: PATCH\n'
-    elif request.method == 'DELETE':
-        return 'ECHO: DELETE\n'
 
 if __name__ == '__main__':
     app.run()
